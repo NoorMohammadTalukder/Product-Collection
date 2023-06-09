a142695774864e37b23fabdd285cecb0ad6ce18f
NoorMohammadTalukder <noor11809027@gmail.com>
2023-06-09 23:29:54 +0600

    token generation,token verfy update

--- helpers/auth.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken');
-function generateToken(userId, secretKey) {
-  const token = jwt.sign({ userId }, secretKey);
-  return token;
-}
-const token = generateToken('demo_user', 'demo_key_for_assignment');
-console.log(token);
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,21 +1,30 @@
+const jwt = require("jsonwebtoken");
 
-const jwt = require('jsonwebtoken');
+//--- generate token
+function generateToken(userId, secretKey) {
+  const token = jwt.sign({ userId }, secretKey);
+  return token;
+}
+const token1 = generateToken("demo_user_id", "demo_key_for_assignment");
+console.log(token1);
+
+//--- verify token
 function authenticate(req, res, next) {
-    const token = req.headers.authorization;
-  
-    if (!token) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-  
-    try {
-      const decoded = jwt.verify(token, 'secretKey');
-      req.userId = decoded.userId;
-      next();
-    } catch (error) {
-        console.log(error)
-      res.status(401).json({ error: 'Unauthorized' });
-    }
+  const token = req.headers.token;
+  // console.token(token);
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
   }
-  
+
+  try {
+    const decoded = jwt.verify(token, "demo_key_for_assignment");
+    // console.log(decoded.userId)
+    // console.log("ok");
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ error: "Unauthorized" });
+  }
+}
 
 module.exports = authenticate;
